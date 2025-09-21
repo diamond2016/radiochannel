39ae1eef45fdd26c0f5a2ca7837937e2bbcc2938
Riccardo Corsi <riccardogcorsi@gmail.com>
2025-09-21 13:54:29 +0200

    implemented RadioStatiionController

---
+++ src/main/java/it/diamondnet/springframework/radiochannel/controller/RadioStationController.java
@@ -0,0 +1,58 @@
+package it.diamondnet.springframework.radiochannel.controller;
+
+import it.diamondnet.springframework.radiochannel.dto.RadioStationDto;
+import it.diamondnet.springframework.radiochannel.service.RadioStationService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/v1/radio-station")
+public class RadioStationController {
+
+    private final RadioStationService radioStationService;
+
+    @GetMapping
+    public Set<RadioStationDto> getRadioStations() {
+        log.debug("List RadioStations - in controller");
+        return radioStationService.getAllRadioStations();
+    }
+
+    @GetMapping("{radioStationId}")
+    public RadioStationDto getRadioStationById(@PathVariable("radioStationId") UUID radioStationId) {
+        log.debug("get RadioStation from id - in controller id: {}", radioStationId);
+        return radioStationService.getRadioStationById(radioStationId);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> handlePost(@RequestBody RadioStationDto radioStation) {
+        RadioStationDto savedRadioStation = radioStationService.saveNewRadioStation(radioStation);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/radio-station" + savedRadioStation.getId().toString());
+
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("{radioStationId}")
+    public ResponseEntity<Void> updateRadioStationById(@PathVariable("radioStationId") UUID radioStationId, @RequestBody RadioStationDto radioStation) {
+        radioStationService.updateRadioStation(radioStationId, radioStation);
+        log.debug("update RadioStation of ID - in controller id: {}", radioStationId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("{radioStationId}")
+    public ResponseEntity<Void> deleteRadioStationById(@PathVariable("radioStationId") UUID radioStationId) {
+        radioStationService.deleteRadioStation(radioStationId);
+        log.debug("delete RadioStation of ID - in controller id: {}", radioStationId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ src/main/java/it/diamondnet/springframework/radiochannel/controller/UserFeedbackController.java
@@ -0,0 +1,58 @@
+package it.diamondnet.springframework.radiochannel.controller;
+
+import it.diamondnet.springframework.radiochannel.dto.UserFeedbackDto;
+import it.diamondnet.springframework.radiochannel.service.UserFeedbackService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/v1/user-feedback")
+public class UserFeedbackController {
+
+    private final UserFeedbackService userFeedbackService;
+
+    @GetMapping
+    public Set<UserFeedbackDto> getUserFeedbacks() {
+        log.debug("List UserFeedbacks - in controller");
+        return userFeedbackService.getAllUserFeedbacks();
+    }
+
+    @GetMapping("{userFeedbackId}")
+    public UserFeedbackDto getUserFeedbackById(@PathVariable("userFeedbackId") UUID userFeedbackId) {
+        log.debug("get UserFeedback from id - in controller id: {}", userFeedbackId);
+        return userFeedbackService.getUserFeedbackById(userFeedbackId);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> handlePost(@RequestBody UserFeedbackDto userFeedback) {
+        UserFeedbackDto savedUserFeedback = userFeedbackService.saveNewUserFeedback(userFeedback);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/user-feedback" + savedUserFeedback.getId().toString());
+
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("{userFeedbackId}")
+    public ResponseEntity<Void> updateUserFeedbackById(@PathVariable("userFeedbackId") UUID userFeedbackId, @RequestBody UserFeedbackDto userFeedback) {
+        userFeedbackService.updateUserFeedback(userFeedbackId, userFeedback);
+        log.debug("update UserFeedback of ID - in controller id: {}", userFeedbackId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("{userFeedbackId}")
+    public ResponseEntity<Void> deleteUserFeedbackById(@PathVariable("userFeedbackId") UUID userFeedbackId) {
+        userFeedbackService.deleteUserFeedback(userFeedbackId);
+        log.debug("delete UserFeedback of ID - in controller id: {}", userFeedbackId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
